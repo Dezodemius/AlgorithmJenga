98dd7c4674b35995c94ca84aa1e94e4f7e08a870
Егор <gladkovyegor@gmail.com>
2022-04-10 10:16:30 +0400

    Init Case2

---
+++ Jenga/Case2.cs
@@ -0,0 +1,11 @@
+﻿namespace Jenga
+{
+  public static class Case2
+  {
+    public static T Do<T>(T[] array, T key)
+    {
+      //поиски
+      return key;
+    }
+  }
+}
---
+++ JengaTests/Case2Tests.cs
@@ -0,0 +1,14 @@
+﻿using NUnit.Framework;
+
+namespace JengaTests
+{
+  [TestFixture]
+  public class Case2Tests
+  {
+    [Test]
+    public void Test1()
+    {
+      
+    }
+  }
+}
