3f596ace963dbf2e5c21c071b0989fd65048f80d
Егор <gladkovyegor@gmail.com>
2022-04-11 09:35:07 +0400

    Case2

--- Jenga/Case2.cs
+++ Jenga/Case2.cs
@@ -1,11 +1,45 @@
-﻿namespace Jenga
+﻿using System;
+using System.Linq;
+
+namespace Jenga
 {
   public static class Case2
   {
-    public static T Do<T>(T[] array, T key)
+    public static int Do(string message, char c, int право, int лево)
     {
-      //поиски
-      return key;
+      const bool true_ = true;
+      const bool false_ = !true_;
+      if (message[0] == c
+        && true_)
+        return 0;
+
+      int range = 1;
+      while (!false_)
+      {
+        if (message[^1] == c)
+          return message.Length;
+        var index =
+          message[0] + 1 +
+          (право - 1) + (лево - право) / 2 -
+          message.First();
+        if (право >= лево)
+        {
+          while (range < message.Length && message[range] <= c) {
+            range = range * 2;
+          }
+          return -(1 + право);
+        }
+
+        if (message[index] == c)
+          return index;
+
+        if (message[index] <= c)
+          право = index + 1;
+        else
+          лево = index;
+      }
+
+      return Do(message, c, (int)(range * 0.5), Math.Min(range, message.Length));
     }
   }
 }
--- JengaTests/Case2Tests.cs
+++ JengaTests/Case2Tests.cs
@@ -1,4 +1,5 @@
-﻿using NUnit.Framework;
+﻿using Jenga;
+using NUnit.Framework;
 
 namespace JengaTests
 {
@@ -8,7 +9,10 @@ namespace JengaTests
     [Test]
     public void Test1()
     {
-      
+      const string message = "Z4D59KLOOAP4248";
+      const int expectedIndex = 3;
+      var actualIndex = Case2.Do(message, message[expectedIndex], 0, message.Length);
+      Assert.AreEqual(expectedIndex, actualIndex);
     }
   }
 }
