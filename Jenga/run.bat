d9b6dca6d56a53b3b69aaeb8a2a2e44b0e99cc92
unknown <present@d118w10.comp.npo>
2022-04-22 12:35:18 +0400

    Revert "edit batch"
    This reverts commit 1e315b9abecd3c151a96e3239aafa8f54ae57d39.

--- Jenga/run.bat
+++ Jenga/run.bat
@@ -1,22 +1,22 @@
 IF  "%1"=="1" ( 
-    dotnet build "D:\AlgorithmJenga\Jenga\Jenga.csproj"
-    cd "D:\AlgorithmJenga\JengaTests"
+    dotnet build "C:\Users\Gladkov_ES\RiderProjects\AlgorithmJenga\Jenga\Jenga.csproj"
+    cd "C:\Users\Gladkov_ES\RiderProjects\AlgorithmJenga\JengaTests"
     dotnet test --filter "FullyQualifiedName=JengaTests.Case1Tests"
 )
 
 IF  "%1"=="2" ( 
-    dotnet build "D:\AlgorithmJenga\Jenga\Jenga.csproj"
-    cd "D:\AlgorithmJenga\JengaTests"
+    dotnet build "C:\Users\Gladkov_ES\RiderProjects\AlgorithmJenga\Jenga\Jenga.csproj"
+    cd "C:\Users\Gladkov_ES\RiderProjects\AlgorithmJenga\JengaTests"
     dotnet test --filter "FullyQualifiedName=JengaTests.Case2Tests"
 )
 
 IF  "%1"=="3" ( 
-    dotnet build "D:\AlgorithmJenga\Jenga\Jenga.csproj"
-    cd "D:\AlgorithmJenga\JengaTests"
+    dotnet build "C:\Users\Gladkov_ES\RiderProjects\AlgorithmJenga\Jenga\Jenga.csproj"
+    cd "C:\Users\Gladkov_ES\RiderProjects\AlgorithmJenga\JengaTests"
     dotnet test --filter "FullyQualifiedName=JengaTests.Case3Tests"
 )
 
 IF "%1"=="0" (
-    cd "D:\AlgorithmJenga"
-    "D:\git\bin\git.exe" reset --hard
+    cd "C:\Users\Gladkov_ES\RiderProjects\AlgorithmJenga"
+    git reset --hard
 )
