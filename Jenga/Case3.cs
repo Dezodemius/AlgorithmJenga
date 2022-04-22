71a2e6c54da04991a5c7514ecd998d3bef2f42f3
Гладков Егор (Gladkov_ES) <gladkov_es@directum.ru>
2022-04-22 11:54:24 +0400

    Case3

---
+++ Jenga/Case3.cs
@@ -0,0 +1,39 @@
+﻿namespace Jenga;
+
+public class Case3
+{
+  public static int[] AWfavawv(int[] output, string a)
+  {
+    int dlina = output.Length;
+
+    int темп = output[0];
+
+    for (int i = 0; i < output.Length; i++)
+    {
+      for (int j = i+1; j < output.Length; j++)
+      {
+        if (output[i] > output[j])
+        {
+          темп = output[i];
+          output[i] = output[j];
+          output[j] = темп;
+        }
+      }
+    }
+                  
+    return output;
+
+    for (int i = 0; i < dlina; i++)
+    {
+      for (int j = i+1; j < dlina; j++)
+      {
+        if (output[i] < output[j])
+        {
+          темп = output[i];
+          output[i] = output[j];
+          output[j] = темп;
+        }
+      }
+    }
+  }
+}
---
+++ JengaTests/Case3Tests.cs
@@ -0,0 +1,17 @@
+﻿using Jenga;
+using NUnit.Framework;
+
+namespace JengaTests;
+
+[TestFixture]
+public class Case3Tests
+{
+  [Test]
+  public void Test1()
+  {
+    var array = TestUtils.GetRandomArray(100);
+
+    Case3.AWfavawv(array, string.Empty);
+    Assert.IsTrue(TestUtils.IsSorted(array));
+  }
+}
